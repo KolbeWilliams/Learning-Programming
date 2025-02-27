69c51124399974cb58ee5eb2c23c584ab6c5f1a7
Kolbe Williams-Wimmer <kolbeleowilliams@gmail.com>
2025-02-27 10:49:56 -0600

    Delete Python/Homeworks directory

--- Python/Homeworks/HW3/Exercise1.py
+++
@@ -1,4 +0,0 @@
-#Exercsie 1: Create a lambda expression that computes the square root of a number
-num = float(input("Enter a number: "))
-result = lambda x : x ** 0.5
-print(f'The square root of {num} is {result(num)}')
